f6ccc3c906267210c76212d1d28f99f6947a1c4c
rohitsudan <rohitcoder94@gmail.com>
2016-09-15 00:08:39 +0530

    upto n digits done

--- Nth_digit_of_PI.py
+++ Nth_digit_of_PI.py
@@ -1,8 +1,20 @@
+from decimal import *
+getcontext().prec = 25 # sets the precision to 25 digits after the decimal
+
 def factorial(n):
 	if n<1:
 		return 1
 	else:
 		return (n*factorial(n-1))
 
-print(factorial(5))
-print(factorial(10))
+def baileyborwein(n):
+	pi = Decimal(0)
+	k=0
+	while(k<n):
+		pi=pi+(Decimal(1)/(16**k))*((Decimal(4)/(8*k+1))-(Decimal(2)/(8*k+4))-(Decimal(1)/(8*k+5))-(Decimal(1)/(8*k+6)))
+		k=k+1
+	#pi = pi * 1/(2**6)
+	return pi
+
+print(baileyborwein(30))
+
